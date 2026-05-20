a278e06878746ba23b0ec243494b8ac878af9b9c
gabriel-kobayashi <gabrielkobayashi11@gmail.com>
2026-05-20 14:32:28 -0300

    feat: configura estrutura inicial do mini ecommerce e cria entidade produto

---
+++ src/main/java/com/gabrieltk/mini_ecommerce/model/Produto.java
@@ -0,0 +1,27 @@
+package com.gabrieltk.mini_ecommerce.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "produtos")
+public class Produto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+    private String descricao;
+    private BigDecimal preco;
+    private Integer estoque;
+}
---
+++ src/test/java/com/gabrieltk/mini_ecommerce/MiniEcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabrieltk.mini_ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniEcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
