af388bc0abff4303404c0f1f5983978e6e746b35
gabriel-kobayashi <gabrielkobayashi11@gmail.com>
2026-05-22 21:15:21 -0300

    feat: adiciona CRUD de categoria na camada controller e exceção personalizada para categoria não encontrada

--- src/main/java/com/gabrieltk/mini_ecommerce/controller/CategoriaController.java
+++ src/main/java/com/gabrieltk/mini_ecommerce/controller/CategoriaController.java
@@ -1,4 +1,52 @@
 package com.gabrieltk.mini_ecommerce.controller;
 
+import com.gabrieltk.mini_ecommerce.dto.CategoriaRequest;
+import com.gabrieltk.mini_ecommerce.dto.CategoriaResponse;
+import com.gabrieltk.mini_ecommerce.service.CategoriaService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/categorias")
 public class CategoriaController {
+
+    private final CategoriaService service;
+
+    public CategoriaController(CategoriaService service) {
+        this.service = service;
+    }
+
+    @PostMapping
+    public ResponseEntity<CategoriaResponse> criar(@Valid @RequestBody CategoriaRequest request) {
+        CategoriaResponse response = service.criar(request);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<CategoriaResponse>> listar() {
+        List<CategoriaResponse> responses = service.listar();
+        return ResponseEntity.ok().body(responses);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<CategoriaResponse> buscarPorId(@PathVariable Long id) {
+        CategoriaResponse response = service.buscarPorId(id);
+        return ResponseEntity.ok(response);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<CategoriaResponse> atualizar(@PathVariable Long id, @RequestBody CategoriaRequest request) {
+        CategoriaResponse response = service.atualizar(id, request);
+        return ResponseEntity.ok(response);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> remover(@PathVariable Long id) {
+        service.remover(id);
+        return ResponseEntity.noContent().build();
+    }
 }
--- src/main/java/com/gabrieltk/mini_ecommerce/exception/CategoriaNotFoundException.java
+++ src/main/java/com/gabrieltk/mini_ecommerce/exception/CategoriaNotFoundException.java
@@ -1,4 +1,7 @@
 package com.gabrieltk.mini_ecommerce.exception;
 
-public class CategoriaNotFoundException {
+public class CategoriaNotFoundException extends RuntimeException {
+    public CategoriaNotFoundException(String msg) {
+        super(msg);
+    }
 }
