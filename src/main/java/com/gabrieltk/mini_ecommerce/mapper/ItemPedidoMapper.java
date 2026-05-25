68ff3e4782eb24df3afd77094bf8962b5948b34a
gabriel-kobayashi <gabrielkobayashi11@gmail.com>
2026-05-25 16:26:44 -0300

    feat: adiciona mappers de pedido

--- src/main/java/com/gabrieltk/mini_ecommerce/mapper/ItemPedidoMapper.java
+++ src/main/java/com/gabrieltk/mini_ecommerce/mapper/ItemPedidoMapper.java
@@ -1,4 +1,17 @@
 package com.gabrieltk.mini_ecommerce.mapper;
 
+import com.gabrieltk.mini_ecommerce.dto.ItemPedidoResponse;
+import com.gabrieltk.mini_ecommerce.model.ItemPedido;
+
 public class ItemPedidoMapper {
+
+    public static ItemPedidoResponse toResponse(ItemPedido itemPedido) {
+        return new ItemPedidoResponse(
+                itemPedido.getId(),
+                itemPedido.getProduto().getNome(),
+                itemPedido.getQuantidade(),
+                itemPedido.getPrecoUnitario(),
+                itemPedido.getSubtotal()
+        );
+    }
 }
--- src/main/java/com/gabrieltk/mini_ecommerce/mapper/PedidoMapper.java
+++ src/main/java/com/gabrieltk/mini_ecommerce/mapper/PedidoMapper.java
@@ -1,4 +1,34 @@
 package com.gabrieltk.mini_ecommerce.mapper;
 
+import com.gabrieltk.mini_ecommerce.dto.ItemPedidoResponse;
+import com.gabrieltk.mini_ecommerce.dto.PedidoRequest;
+import com.gabrieltk.mini_ecommerce.dto.PedidoResponse;
+import com.gabrieltk.mini_ecommerce.model.Pedido;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
 public class PedidoMapper {
+
+    public static Pedido toEntity(PedidoRequest request) {
+        Pedido pedido = new Pedido();
+        pedido.setDataPedido(LocalDateTime.now());
+
+        return pedido;
+    }
+
+    public static PedidoResponse toResponse(Pedido pedido) {
+        List<ItemPedidoResponse> itens =
+                pedido.getItens()
+                        .stream()
+                        .map(ItemPedidoMapper::toResponse)
+                        .toList();
+
+        return new PedidoResponse(
+                pedido.getId(),
+                pedido.getDataPedido(),
+                pedido.getValorTotal(),
+                itens
+        );
+    }
 }
